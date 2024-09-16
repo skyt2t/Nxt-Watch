1f8236cfc5445faef401b94b021dc95609a44eeb
skyt2t <skyasar210@gmail.com>
2024-09-16 09:51:50 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -16,8 +16,8 @@ import {
 
 class Login extends Component {
   state = {
-    username: '',
-    password: '',
+    username: 'rahul',
+    password: 'rahul@2021',
     showErrMsg: false,
     errorMsg: '',
     type: 'password',
